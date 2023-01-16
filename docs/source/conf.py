f1c0b99b2c0ebb441db03057b120e46cf704d750
Christian Chapman-Bird <c.chapman-bird.1@research.gla.ac.uk>
2023-01-16 12:29:46 +0000

    change to rtd theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,3 +1,4 @@
+import sphinx_rtd_theme
 # Configuration file for the Sphinx documentation builder.
 #
 # For the full list of built-in configuration values, see the documentation:
@@ -14,7 +15,9 @@ release = '0.1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+        'sphinx_rtd_theme'
+]
 
 templates_path = ['_templates']
 exclude_patterns = []
@@ -24,5 +27,5 @@ exclude_patterns = []
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
