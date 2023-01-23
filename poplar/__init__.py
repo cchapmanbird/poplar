9d0b7b9e84cb747a2297b83fe861917f9b6355d5
Christian Chapman-Bird <c.chapman-bird.1@research.gla.ac.uk>
2023-01-23 15:04:14 +0000

    dynamic versioning

--- poplar/__init__.py
+++ poplar/__init__.py
@@ -1,11 +1,7 @@
 """poplar is a package for modelling selection biases with machine learning, and performing hyperparameter estimation.
 """
 
-try:
-    from importlib.metadata import version, PackageNotFoundError
-except ImportError:  # for Python < 3.8
-    from importlib_metadata import version, PackageNotFoundError
-
+from importlib.metadata import version, PackageNotFoundError
 
 try:
     __version__ = version(__name__)
